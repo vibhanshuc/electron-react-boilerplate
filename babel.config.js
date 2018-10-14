478dede8416a0ececd9807caa95fd065f267a41e
vikr01 <vik120@icloud.com>
2018-10-14 00:14:39 -0700

    use function babel config

--- babel.config.js
+++ babel.config.js
@@ -1,75 +1,69 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: { node: 10 },
-        useBuiltIns: 'usage'
-      }
+/* eslint global-require: 0 */
+
+const devPlugins = [
+  // require('babel-plugin-flow-runtime')
+];
+
+const prodPlugins = [
+  require('babel-plugin-dev-expression'),
+
+  // Formerly babel-preset-react-optimize
+  require('@babel/plugin-transform-react-constant-elements'),
+  require('@babel/plugin-transform-react-inline-elements')
+  // require('babel-plugin-transform-react-remove-prop-types'), // POSSIBLY UNSTABLE
+  // require('babel-plugin-transform-react-pure-class-to-function') // POSSIBLY UNSTABLE
+];
+
+module.exports = api => {
+  const isDevEnv = api.env(['development', 'test']);
+
+  return {
+    presets: [
+      [
+        require('@babel/preset-env'),
+        {
+          targets: { node: 10 },
+          useBuiltIns: 'usage'
+        }
+      ],
+      require('@babel/preset-flow'),
+      [
+        require('@babel/preset-react'),
+        { development: api.env(['development, test']) }
+      ]
     ],
-    '@babel/preset-flow'
-  ],
-  plugins: [
-    // Stage 0
-    '@babel/plugin-proposal-function-bind',
+    plugins: [
+      ...(isDevEnv ? devPlugins : prodPlugins),
 
-    // Stage 1
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-logical-assignment-operators',
-    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-    '@babel/plugin-proposal-do-expressions',
+      // Stage 0
+      require('@babel/plugin-proposal-function-bind'),
 
-    // Stage 2
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
+      // Stage 1
+      require('@babel/plugin-proposal-export-default-from'),
+      require('@babel/plugin-proposal-logical-assignment-operators'),
+      [require('@babel/plugin-proposal-optional-chaining'), { loose: false }],
+      [
+        require('@babel/plugin-proposal-pipeline-operator'),
+        { proposal: 'minimal' }
+      ],
+      [
+        require('@babel/plugin-proposal-nullish-coalescing-operator'),
+        { loose: false }
+      ],
+      require('@babel/plugin-proposal-do-expressions'),
 
-    // Stage 3
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-json-strings'
-  ],
-  env: {
-    production: {
-      presets: ['@babel/preset-react'],
-      plugins: [
-        'dev-expression',
+      // Stage 2
+      [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+      require('@babel/plugin-proposal-function-sent'),
+      require('@babel/plugin-proposal-export-namespace-from'),
+      require('@babel/plugin-proposal-numeric-separator'),
+      require('@babel/plugin-proposal-throw-expressions'),
 
-        // Formerly babel-preset-react-optimize
-        '@babel/plugin-transform-react-constant-elements',
-        '@babel/plugin-transform-react-inline-elements'
-        // 'babel-plugin-transform-react-remove-prop-types', // POSSIBLY UNSTABLE
-        // 'babel-plugin-transform-react-pure-class-to-function' // POSSIBLY UNSTABLE
-      ]
-    },
-    development: {
-      presets: [['@babel/preset-react', { development: true }]],
-      plugins: [
-        // [
-        //   'flow-runtime',
-        //   {
-        //     assert: true,
-        //     annotate: true
-        //   }
-        // ]
-      ]
-    },
-    test: {
-      presets: [['@babel/preset-react', { development: true }]],
-      plugins: [
-        // [
-        //   'flow-runtime',
-        //   {
-        //     assert: true,
-        //     annotate: true
-        //   }
-        // ]
-      ]
-    }
-  }
+      // Stage 3
+      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-syntax-import-meta'),
+      [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+      require('@babel/plugin-proposal-json-strings')
+    ]
+  };
 };
