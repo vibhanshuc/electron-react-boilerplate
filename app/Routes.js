82142847c5e22107b709f57ddf8b8c51cf1532fc
Vibhanshu <vibhanshu86@gmail.com>
2019-02-08 14:00:06 +0530

    cleanup

--- app/Routes.js
+++ app/Routes.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
+      <Route
+        path={routes.HOME}
+        component={() => (
+          <div>
+            Home
+            {process.env.BASE_API}
+          </div>
+        )}
+      />
     </Switch>
   </App>
 );
